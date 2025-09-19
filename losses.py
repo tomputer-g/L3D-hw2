72b924cd0d647492922bb9362720b2197160a0fe
zimingg <zimingg2001@gmail.com>
2025-09-19 11:13:37 -0400

    Voxel loss using BCELoss

--- losses.py
+++ losses.py
@@ -1,13 +1,16 @@
 import torch
 
 # define losses
-def voxel_loss(voxel_src,voxel_tgt):
+def voxel_loss(voxel_src, voxel_tgt):
 	# voxel_src: b x h x w x d
 	# voxel_tgt: b x h x w x d
 	# loss = 
 	# implement some loss for binary voxel grids
- 
 	# Can use predefined loss from pytorch
+	# Maximize log likelihod L06 P49
+	# -wn (y log x + (1-y) log(1-x))
+	loss_fn = torch.nn.BCELoss(reduction='mean')
+	loss = loss_fn(voxel_src, voxel_tgt)
 	return loss
 
 def chamfer_loss(point_cloud_src,point_cloud_tgt):
