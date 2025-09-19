b72252fbe95f3f1be9c152bd421ada829015aa08
zimingg <zimingg2001@gmail.com>
2025-09-19 11:35:40 -0400

    Waiting to run chamfer loss on cuda device

--- losses.py
+++ losses.py
@@ -1,4 +1,5 @@
 import torch
+from pytorch3d.ops.knn import knn_points, knn_gather
 
 # define losses
 def voxel_loss(voxel_src, voxel_tgt):
@@ -17,6 +18,18 @@ def chamfer_loss(point_cloud_src,point_cloud_tgt):
 	# point_cloud_src, point_cloud_src: b x n_points x 3  
 	# loss_chamfer = 
 	# implement chamfer loss from scratch
+ 
+	dists, idx, nn = knn_points(p1=point_cloud_src, p2=point_cloud_tgt, lengths1=None, lengths2=None, norm=2, K=1, return_nn=False)
+	# nn[n, i, k] gives kth nn for p1[n, i]
+	L2_src = torch.sum(dists)
+ 
+	dists, idx, nn = knn_points(p1=point_cloud_tgt, p2=point_cloud_src, lengths1=None, lengths2=None, norm=2, K=1, return_nn=False)
+	L2_dst = torch.sum(dists)
+ 
+	loss_chamfer = L2_src + L2_dst
+	# L2_src = torch.linalg.norm(point_cloud_src - nn)
+
+	# sum over points in S1 of L2 dist to closest point in S2, then other way around
 
 	#Can use pytorch3d.ops.knn.knn_gather or knn_points
 	return loss_chamfer
