35e33ca74ec55c9beaa7c34c67b7c3ceb0497fbd
zimingg <zimingg2001@gmail.com>
2025-09-19 10:09:26 -0400

    Initial

--- losses.py
+++ losses.py
@@ -6,15 +6,21 @@ def voxel_loss(voxel_src,voxel_tgt):
 	# voxel_tgt: b x h x w x d
 	# loss = 
 	# implement some loss for binary voxel grids
+ 
+	# Can use predefined loss from pytorch
 	return loss
 
 def chamfer_loss(point_cloud_src,point_cloud_tgt):
 	# point_cloud_src, point_cloud_src: b x n_points x 3  
 	# loss_chamfer = 
 	# implement chamfer loss from scratch
+
+	#Can use pytorch3d.ops.knn.knn_gather or knn_points
 	return loss_chamfer
 
 def smoothness_loss(mesh_src):
 	# loss_laplacian = 
 	# implement laplacian smoothening loss
+
+	# Can use predefined loss from pytorch
 	return loss_laplacian
